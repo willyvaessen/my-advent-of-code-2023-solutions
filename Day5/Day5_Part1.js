ff37d2f2c5ed9e1ee71b04483d1d3d0c252e297f
WillyVaessen <github@willyvaessen.nl>
2023-12-05 11:20:31 +0100

    Day 5 preparation

--- Day5/Day5_Part1.js
+++ Day5/Day5_Part1.js
@@ -19,88 +19,138 @@ console.log(INPUT[2]);
 
 
 function getMaps(inputData) {
-    console.log("--- Creating maps from input ---");
-    console.log("--- Seed to Soil----------------------------------------------------------------");
+    let dstRangeStart = 0;  //  Destination Range Start (First value in a line)
+    let srcRangeStart = 0;  //  Source Range Start      (Second value in a line)
+    let rangeLength = 0;    //  Length of the Ranges    (Third value in a line)
+    // console.log("--- Creating maps from input ---");
+    // console.log("--- Seed to Soil----------------------------------------------------------------");
     let dataStart = INPUT.indexOf('seed-to-soil map:') + 1;
     let dataEnd = INPUT.indexOf('soil-to-fertilizer map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        seedToSoil.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
 
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
+    // console.log("--- Soil to Fertilizer ---------------------------------------------------------");
+    dataStart = INPUT.indexOf('soil-to-fertilizer map:') + 1;
+    dataEnd = INPUT.indexOf('fertilizer-to-water map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        soilToFertilizer.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
+
+    // console.log("--- Fertilizer to Water --------------------------------------------------------");
+    dataStart = INPUT.indexOf('fertilizer-to-water map:') + 1;
+    dataEnd = INPUT.indexOf('water-to-light map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        fertilizerToWater.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
+
+    // console.log("--- Water to Light -------------------------------------------------------------");
+    dataStart = INPUT.indexOf('water-to-light map:') + 1;
+    dataEnd = INPUT.indexOf('light-to-temperature map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        waterToLight.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
+
+    // console.log("--- Light to Temperature -------------------------------------------------------");
+    dataStart = INPUT.indexOf('light-to-temperature map:') + 1;
+    dataEnd = INPUT.indexOf('temperature-to-humidity map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        lightToTemperature.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
+
+    // console.log("--- Temperature to Humidity ----------------------------------------------------");
+    dataStart = INPUT.indexOf('temperature-to-humidity map:') + 1;
+    dataEnd = INPUT.indexOf('humidity-to-location map:') - 2;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        temperatureToHumidity.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
+
+    // console.log("--- Humidity to Location -------------------------------------------------------");
+    dataStart = INPUT.indexOf('humidity-to-location map:') + 1;
+    dataEnd = INPUT.length - 1;
+    for (let i = dataStart; i <= dataEnd; i++) {
+        console.log(INPUT[i].split(' ')[0]);
+        humidityToLocation.push({
+            "Destination": INPUT[i].split(' ')[0],
+            "Source": INPUT[i].split(' ')[1],
+            "Length": INPUT[i].split(' ')[2]
+        });
+    }
 
 
+    // console.log("--------------------------------------------------------------------------------");
+    // console.log("");
+
+
+}   //  This function extracts the INPUT into arrays to work with.
+
+function logArrays() {      //  A function to log all the arrays that have been created
+    console.log("--- Seeds ----------------------------------------------------------------------");
+    console.log(SEEDS);
+    console.log("--------------------------------------------------------------------------------");
+    console.log("");
+    console.log("--- Seed to Soil----------------------------------------------------------------");
+    console.log(seedToSoil);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
     console.log("--- Soil to Fertilizer ---------------------------------------------------------");
-    dataStart = INPUT.indexOf('soil-to-fertilizer map:') + 1;
-    dataEnd = INPUT.indexOf('fertilizer-to-water map:') - 2;
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
-
+    console.log(soilToFertilizer);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
     console.log("--- Fertilizer to Water --------------------------------------------------------");
-    dataStart = INPUT.indexOf('fertilizer-to-water map:') + 1;
-    dataEnd = INPUT.indexOf('water-to-light map:') - 2;
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
-
+    console.log(fertilizerToWater);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
     console.log("--- Water to Light -------------------------------------------------------------");
-    dataStart = INPUT.indexOf('water-to-light map:') + 1;
-    dataEnd = INPUT.indexOf('light-to-temperature map:') - 2;
-
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
-
+    console.log(waterToLight);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
-
     console.log("--- Light to Temperature -------------------------------------------------------");
-    dataStart = INPUT.indexOf('light-to-temperature map:') + 1;
-    dataEnd = INPUT.indexOf('temperature-to-humidity map:') - 2;
-
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
+    console.log(lightToTemperature);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
     console.log("--- Temperature to Humidity ----------------------------------------------------");
-    dataStart = INPUT.indexOf('temperature-to-humidity map:') + 1;
-    dataEnd = INPUT.indexOf('humidity-to-location map:') - 2;
-
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
+    console.log(temperatureToHumidity);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
     console.log("--- Humidity to Location -------------------------------------------------------");
-    dataStart = INPUT.indexOf('humidity-to-location map:') + 1;
-    dataEnd = INPUT.length -1;
-
-
-    console.log(`First line of data is ${dataStart}, last line is ${dataEnd}`)
-    console.log(`First Line: ${INPUT[dataStart]}`);
-    console.log(`Last Line: ${INPUT[dataEnd]}`);
+    console.log(humidityToLocation);
     console.log("--------------------------------------------------------------------------------");
     console.log("");
 
-
-}
-
+}          //  A function to log all the arrays that have been created
+//  logArrays();
 
 getMaps(INPUT);
 
 
-// console.log("--- Seeds ----------------------------------------------------------------------");
-// console.log(SEEDS);
-// console.log("--------------------------------------------------------------------------------");
