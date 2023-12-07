74dc613e639ca1721772a66d54eba0b9ecdaa28d
WillyVaessen <github@willyvaessen.nl>
2023-12-07 18:08:46 +0100

    Day 7, part 2.
    That's the right answer! You are one gold star closer to restoring snow operations.
    
    You have completed Day 7!

--- Day7/Day7_Part1.js
+++ Day7/Day7_Part1.js
@@ -1,7 +1,6 @@
 //  First get the input:
 const fs = require('fs');
-// const INPUT = fs.readFileSync('./Day7_Input', 'utf-8').split('\n');
-const INPUT = fs.readFileSync('./Day7_Input_Example', 'utf-8').split('\n');
+const INPUT = fs.readFileSync('./Day7_Input', 'utf-8').split('\n');
 const DECK = {
     'A': 13,
     'K': 12,
@@ -20,42 +19,28 @@ const DECK = {
 // console.log(INPUT);
 const HANDS = [];
 
-//  Define functions
 function splitHands(input) {
     for (let i = 0; i < input.length; i++) {
-        // console.log(input[i]);
         let hand = input[i].split(' ')[0];
         let bidAmount = input[i].split(' ')[1];
-        // console.log(`Hand: ${hand}  ${bidAmount}`);
         HANDS.push({hand, bidAmount});
     }
 }
 
 function countCards(hand) {
-    // Initialize an empty object to store card counts
     const cardCount = {};
-
-    // Loop through each card in the hand
     for (let i = 0; i < hand.length; i++) {
         const card = hand[i];
-
-        // Check if the card is already in the object
         if (cardCount[card]) {
-            // If it's present, increment the count
             cardCount[card]++;
         } else {
-            // If it's not present, add it to the object with a count of 1
             cardCount[card] = 1;
         }
     }
-
-    // Output the final card count object
-    // console.log(cardCount);
     return cardCount;
 }
 
 function getHighestCount(handCount) {
-    // Find the entry with the lowest count
     let maxCount = 1;
     let maxCard = null;
 
@@ -66,8 +51,6 @@ function getHighestCount(handCount) {
             maxCard = card;
         }
     }
-    // console.log(`Card with the highest count: ${maxCard} (${maxCount} occurrences)`);
-    // console.log(handCount);
     return maxCount;
 }
 
@@ -77,112 +60,75 @@ function getHandType(hand) {
     let handType = '';
     let typeRank = 0;
     let amount = 0;
-    // console.log("*** Getting the hand type ***");
     handCount = countCards(HAND)
     const handCountSize = Object.keys(handCount).length;
-    // console.log(`The hand ${HAND} contains the ${handCountSize} unique cards.`);
-    // console.log(`Checking the hand type of hand ${HAND}.`)
     if (handCountSize === 1) {
-        // console.log(`Only 1 type of card in hand ${HAND}, this must be Five of a Kind`);
         handType = 'fiveOfKind';
         typeRank = 7;
     } else if (handCountSize === 2) {
-        // console.log(`There are 2 types of card in hand ${HAND}, this can be Four of a Kind or a Full House`);
-        // console.log(getHighestCount(handCount));
-        // console.log("---------- ---------- ---------- ----------");
         if (getHighestCount(handCount) === 3) {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so type is Full House`);
             handType = 'fullHouse';
             typeRank = 5;
         } else {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so type is Four of a Kind`);
             handType = 'fourOfKind';
             typeRank = 6;
         }
     } else if (handCountSize === 3) {
-        // console.log(`There are 3 types of card in hand ${HAND}, this can be Three of a Kind or Two Pairs`);
         if (getHighestCount(handCount) === 2) {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so type is Two Pairs`);
             handType = 'twoPairs';
             typeRank = 3;
         } else {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so Three of a Kind`);
             handType = 'threeOfKind';
             typeRank = 4;
         }
     } else if (handCountSize === 4) {
-        // console.log(`There are 4 types of card in hand ${HAND}, this must be One Pair`);
         handType = 'onePair';
         typeRank = 2;
     } else if (handCountSize === 5) {
-        // console.log(`Five different cards in hand ${HAND}, this must be a High Card`);
         handType = 'highCard';
         typeRank = 1;
-    } else {
-        // console.log(`This is a different type of hand, or an error.`)
     }
-    // console.log(`The hand ${HAND} is a ${handType}`);
     return [handType, typeRank];
 }
 
-
-// getHandType(splitHands(INPUT));
-
-
 function main() {
     console.log("*** Running Program ***");
-    // console.log(HANDS);
     splitHands(INPUT);
-    // console.log(HANDS);
 }
-
 
 //  Run the program
 main();
 for (let i = 0; i < HANDS.length; i++) {
     let currentHand = HANDS[i];
-    // console.log(currentHand)
-    // console.log(getHandType(currentHand)[0]);
     HANDS[i].type = getHandType(currentHand)[0];
     HANDS[i].typeRank = getHandType(currentHand)[1];
-
-    // console.log(HANDS);
 }
 
 const handsSortedByType = HANDS.sort((a, b) => {
-  // Compare by typeRank
-  if (a.typeRank !== b.typeRank) {
-    return a.typeRank - b.typeRank;
-  }
-
-  // If typeRank is the same, compare by hand using DECK order
-  const compareHands = (handA, handB) => {
-    for (let i = 0; i < Math.min(handA.length, handB.length); i++) {
-      const rankA = DECK[handA[i]];
-      const rankB = DECK[handB[i]];
-
-      if (rankA !== rankB) {
-        return rankA - rankB;
-      }
+    if (a.typeRank !== b.typeRank) {
+        return a.typeRank - b.typeRank;
     }
-
-    return handA.length - handB.length;
-  };
-
-  return compareHands(a.hand, b.hand);
+    const compareHands = (handA, handB) => {
+        for (let i = 0; i < Math.min(handA.length, handB.length); i++) {
+            const rankA = DECK[handA[i]];
+            const rankB = DECK[handB[i]];
+            if (rankA !== rankB) {
+                return rankA - rankB;
+            }
+        }
+        return handA.length - handB.length;
+    };
+    return compareHands(a.hand, b.hand);
 });
 
 console.log(handsSortedByType);
 let totalWinnings = 0;
 
-console.log("*** Final Answer:")
 for (let i = 0; i < handsSortedByType.length; i++) {
-    let rank = i+1;
+    let rank = i + 1;
     let bid = handsSortedByType[i].bidAmount;
     let winning = rank * bid;
-    // console.log(`Index ${i}, rank ${rank}, ${bid}. Winning is ${rank} x ${bid} = ${winning}`);
     totalWinnings += winning;
-
 }
 console.log("*** Final Answer:")
-    console.log(totalWinnings);
+console.log(totalWinnings);
--- Day7/Day7_Part2.js
+++ Day7/Day7_Part2.js
@@ -1,7 +1,5 @@
-//  First get the input:
 const fs = require('fs');
 const INPUT = fs.readFileSync('./Day7_Input', 'utf-8').split('\n');
-// const INPUT = fs.readFileSync('./Day7_Input_Example', 'utf-8').split('\n');
 const DECK = {
     'A': 13,
     'K': 12,
@@ -17,161 +15,94 @@ const DECK = {
     '2': 2,
     'J': 1,
 }
-// console.log(INPUT);
 const HANDS = [];
 
 //  Define functions
 function splitHands(input) {
     for (let i = 0; i < input.length; i++) {
-        // console.log(input[i]);
         let hand = input[i].split(' ')[0];
         let bidAmount = input[i].split(' ')[1];
-        // console.log(`Hand: ${hand}  ${bidAmount}`);
         HANDS.push({hand, bidAmount});
     }
 }
 
 function countCards(hand) {
-    // Initialize an empty object to store card counts
     const cardCount = {};
 
-    // Loop through each card in the hand
     for (let i = 0; i < hand.length; i++) {
         const card = hand[i];
 
-        // Check if the card is already in the object
         if (cardCount[card]) {
-            // If it's present, increment the count
             cardCount[card]++;
         } else {
-            // If it's not present, add it to the object with a count of 1
             cardCount[card] = 1;
         }
     }
-
-    // Output the final card count object
-    // console.log(cardCount);
     return cardCount;
 }
 
 function getHighestCount(handCount) {
-    // Find the entry with the lowest count
     let maxCount = 1;
     let maxCard = null;
 
-    // Iterate over the keys of the cardCount object
     for (const card in handCount) {
         if (handCount[card] > maxCount) {
             maxCount = handCount[card];
             maxCard = card;
         }
     }
-    // console.log(`Card with the highest count: ${maxCard} (${maxCount} occurrences)`);
-    // console.log(handCount);
     return maxCount;
 }
 
 function getHighestCountWithJokers(handCount) {
-    // Find the entry with the lowest count
     let maxCount = 1;
     let maxCard = null;
 
-    // Iterate over the keys of the cardCount object
     for (const card in handCount) {
         if (handCount[card] > maxCount) {
             maxCount = handCount[card];
-            console.log("");
-            console.log("++++++++++")
-            console.log(`Card is: ${card}`);
-            console.log("==========")
             maxCard = card;
         } else if (handCount[card] === maxCount) {
             maxCard = card;
         }
     }
-    // console.log(`Card with the highest count: ${maxCard} (${maxCount} occurrences)`);
-    // console.log(handCount);
     return [maxCard, maxCount];
 }
 
 
 function handleJokers(hand) {
     let joker = hand.J;
-    // console.log(hand);
     if (joker === undefined) {
-        console.log(`No Jokers in this hand.`);
     } else {
-        console.log(`Number of jokers in this hand is ${joker}`);
         let maxCard = getHighestCountWithJokers(hand)[0];
         let highestCount = getHighestCountWithJokers(hand)[1];
-        // console.log(maxCard, getHighestCountWithJokers);
-        // console.log("");
-        // console.log("|**----------|");
-        // console.log(hand[maxCard]);
-        // console.log("|----------|");
-        // console.log("*** Updating amount of highest card:")
         if (hand.J === 5) {
-            // console.log("*** Updating amount of highest card:")
             hand[maxCard] += hand.J;
-            // hand.J = 0;
-            // delete hand['J'];
-            // console.log("|----------|");
-            // console.log(hand[maxCard]);
-            // console.log("|----------|");
         } else {
-            console.log("__________");
-            console.log(hand);
-            console.log(maxCard);
-            console.log("**********");
-            console.log("");
-            console.log("");
             hand[maxCard] += hand.J;
             hand.J = 0;
             delete hand['J'];
-            // console.log("|----------|");
-            // console.log(hand[maxCard]);
-            // console.log("|----------|");
-
         }
     }
-
-    console.log("##########");
-    console.log(hand);
-    console.log("**********");
-    console.log("");
-    console.log("");
     return hand;
 }
 
 
 function getHandType(hand) {
-    // console.log("*** Getting Hand Types ***");
     let handCount = [];
     const HAND = hand.hand;
     let handType = '';
     let typeRank = 0;
     let amount = 0;
-    // console.log("*** Getting the hand count ***");
-    // console.log(HAND);
     handCount = countCards(HAND)
-    // console.log(handCount);
 
-    // console.log("Handling the Jokers in the hand");
     handleJokers(handCount);
-    // console.log("***----------***----------***");
     const handCountSize = Object.keys(handCount).length;
-    // console.log(`The hand ${HAND} contains the ${handCountSize} unique cards.`);
-    // console.log(`Checking the hand type of hand ${HAND}.`)
     if (handCountSize === 1) {
-        // console.log(`Only 1 type of card in hand ${HAND}, this must be Five of a Kind`);
         handType = 'fiveOfKind';
         typeRank = 7;
     } else if (handCountSize === 2) {
-        // console.log(`There are 2 types of card in hand ${HAND}, this can be Four of a Kind or a Full House`);
-        // console.log(getHighestCount(handCount));
-        // console.log("---------- ---------- ---------- ----------");
         if (getHighestCount(handCount) === 3) {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so type is Full House`);
             handType = 'fullHouse';
             typeRank = 5;
         } else {
@@ -180,62 +111,44 @@ function getHandType(hand) {
             typeRank = 6;
         }
     } else if (handCountSize === 3) {
-        // console.log(`There are 3 types of card in hand ${HAND}, this can be Three of a Kind or Two Pairs`);
         if (getHighestCount(handCount) === 2) {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so type is Two Pairs`);
             handType = 'twoPairs';
             typeRank = 3;
         } else {
-            // console.log(`Highest count is ${getHighestCount(handCount)}, so Three of a Kind`);
             handType = 'threeOfKind';
             typeRank = 4;
         }
     } else if (handCountSize === 4) {
-        // console.log(`There are 4 types of card in hand ${HAND}, this must be One Pair`);
         handType = 'onePair';
         typeRank = 2;
     } else if (handCountSize === 5) {
-        // console.log(`Five different cards in hand ${HAND}, this must be a High Card`);
         handType = 'highCard';
         typeRank = 1;
     } else {
-        // console.log(`This is a different type of hand, or an error.`)
     }
-    // console.log(`The hand ${HAND} is a ${handType}`);
     return [handType, typeRank];
 }
 
 
 function main() {
     console.log("*** Running Program ***");
-    // console.log(HANDS);
     splitHands(INPUT);
-    // console.log(HANDS);
 }
 
 
 //  Run the program
 main();
-console.log()
-
 
 for (let i = 0; i < HANDS.length; i++) {
     let currentHand = HANDS[i];
-    // console.log(currentHand)
-    // console.log(getHandType(currentHand)[0]);
     HANDS[i].type = getHandType(currentHand)[0];
     HANDS[i].typeRank = getHandType(currentHand)[1];
-
-    // console.log(HANDS);
 }
 
 const handsSortedByType = HANDS.sort((a, b) => {
-    // Compare by typeRank
     if (a.typeRank !== b.typeRank) {
         return a.typeRank - b.typeRank;
     }
-
-    // If typeRank is the same, compare by hand using DECK order
     const compareHands = (handA, handB) => {
         for (let i = 0; i < Math.min(handA.length, handB.length); i++) {
             const rankA = DECK[handA[i]];
@@ -252,22 +165,14 @@ const handsSortedByType = HANDS.sort((a, b) => {
     return compareHands(a.hand, b.hand);
 });
 
-console.log(handsSortedByType);
 let totalWinnings = 0;
 
-console.log("*** Final Answer:")
 for (let i = 0; i < handsSortedByType.length; i++) {
     let rank = i + 1;
     let bid = handsSortedByType[i].bidAmount;
     let winning = rank * bid;
-    // console.log(`Index ${i}, rank ${rank}, ${bid}. Winning is ${rank} x ${bid} = ${winning}`);
     totalWinnings += winning;
 
 }
 console.log("*** Final Answer:")
 console.log(totalWinnings);
-
-//  Not:
-//  249785241 (too high)
-//  250015282 (too high)
-//  249620106
