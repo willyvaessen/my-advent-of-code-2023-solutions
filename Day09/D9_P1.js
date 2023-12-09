0be990ebdaaa74f8f9f82c360e9f3f28d2a043a1
WillyVaessen <github@willyvaessen.nl>
2023-12-09 13:10:49 +0100

    Started day 9. Got it working now, with the example input. Saving, now time to run it with the actual input.

--- Day09/D9_P1.js
+++ Day09/D9_P1.js
@@ -6,7 +6,7 @@ const INPUT = fs.readFileSync('./Day9_Input_Example', 'utf-8').split('\n');
 // const sequence = INPUT[0].split(' ');
 let run = 0;
 let allZeros = false;
-
+let sumOfExtraPolatedValues = 0;
 
 function createFirstCollection(array) {
     const collection = {};
@@ -41,21 +41,65 @@ function calcSteps(collection) {
 }
 
 function getPrediction(collection) {
-    console.log(collection);
+    // console.log(collection);
+    // console.log(run)
+    let valueToAdd;
+    while (run > 0) {
+        // console.log(collection[run]);
+        // console.log(`This collection has a length of ${collection[run].length}`);
+        //  Check to see if all values are 0
+        if (collection[run].every(value => value === 0)) {
+            valueToAdd = 0;
+            // console.log(`All values are 0. Adding a zero to the end.`);
+            collection[run].push(valueToAdd);
+            // console.log(collection[run]);
+            // console.log(collection[run].length);
+
+        } else {
+            // console.log("We'll do something else.");
+            let currentLastValue = collection[run][collection[run].length - 1];
+            valueToAdd = collection[run + 1][collection[run + 1].length - 1] + currentLastValue;
+            // console.log(`Last value in this collection is ${currentLastValue}. Value to add is ${valueToAdd}.`);
+            collection[run].push(valueToAdd);
+        }
+
+        run--;
+    }
+    // console.log(`Original collection for run ${run} is:`);
+    // console.log(collection[run]);
+    let currentLastValue = collection[run][collection[run].length - 1];
+    // console.log(`Last value in this collection is ${currentLastValue}`);
+    valueToAdd = collection[run + 1][collection[run + 1].length - 1] + currentLastValue;
+    // console.log(`Last value in this collection is ${currentLastValue}. Value to add is ${valueToAdd}.`);
+    collection[run].push(valueToAdd);
+    let predictedValue = collection[run][collection[run].length-1];
+
+
+    // collection[run].push(0);
+    // // console.log(collection[run].indexOf(0))
+    // console.log(collection[run][4]);
+    // console.log(collection[run])
+    // collection[run -1].push('A')
+    // console.log(collection[run -1][collection[run -1].length-1])
+    // collection[run -2].push('B')
+    // console.log(collection[run -1][collection[run -2].length-1])
+    // console.log(collection)
+    // console.log(`Prediction is: ${predictedValue}.`)
+    sumOfExtraPolatedValues += predictedValue;
 }
 
 
 function handleSingleLine(line) {
     allZeros = false;
-        run = 0;
-        console.log(`Handling line ${line + 1} of the input`);
-        const collection = createFirstCollection(INPUT[line].split(' '));
-        while (!allZeros) {
-            calcSteps(collection);
-        }
-        // console.log(`Until this point (run ${run}) The following collections have been created. Now to work our way back up.`)
-        // console.log(collection);
-        getPrediction(collection);
+    run = 0;
+    console.log(`Handling line ${line + 1} of the input`);
+    const collection = createFirstCollection(INPUT[line].split(' '));
+    while (!allZeros) {
+        calcSteps(collection);
+    }
+    // console.log(`Until this point (run ${run}) The following collections have been created. Now to work our way back up.`)
+    // console.log(collection);
+    getPrediction(collection);
 }
 
 
@@ -79,5 +123,6 @@ function main() {
 }
 
 
-// main();
-handleSingleLine(0);
+main();
+// handleSingleLine(2);
+console.log(sumOfExtraPolatedValues);
