824ac0c973e82408233760a332423d0bed64d143
WillyVaessen <github@willyvaessen.nl>
2023-12-12 15:51:45 +0100

    FINALLY!!!
    That's the right answer! You are one gold star closer to restoring snow operations.

--- Day11/Day11Part1.js
+++ Day11/Day11Part1.js
@@ -1,7 +1,7 @@
 //  First get the input:
 const fs = require('fs');
-// const INPUT = fs.readFileSync('./Day11_Input', 'utf-8').split('\n');
-const INPUT = fs.readFileSync('./Day11_Input_Example', 'utf-8').split('\n');
+const INPUT = fs.readFileSync('./Day11_Input', 'utf-8').split('\n');
+// const INPUT = fs.readFileSync('./Day11_Input_Example', 'utf-8').split('\n');
 // console.log(INPUT)
 const galaxyMarker = '#';
 const galaxyMap = {};
@@ -140,7 +140,8 @@ function getGalaxyMap(universe) {
     for (let row = 0; row < universe.length; row++) {
         for (let col = 0; col < universe[row].length; col++) {
             if (universe[row][col] === galaxyMarker) {
-                console.log(`Galaxy ${galaxyCounter} found at ${row}x${col}.`)
+                // console.log(`Galaxy ${galaxyCounter} found at ${row}x${col}.`)
+                // fs.appendFileSync('map.txt', `Galaxy ${galaxyCounter} found at ${row}x${col}.\n`);
                 galaxyMap[galaxyCounter] = [row, col];
                 galaxyCounter++;
             }
@@ -149,7 +150,7 @@ function getGalaxyMap(universe) {
 }
 
 getGalaxyMap(expandedUniverse);
-console.log(galaxyMap);
+console.log(galaxyMap['1'][0]);
 
 function getGalaxyPairs() {
     const galaxies = [];
@@ -172,20 +173,21 @@ function getGalaxyPairs() {
 const galaxyPairs = getGalaxyPairs();
 
 
-console.log(galaxyPairs[1][0]);
-
 function calculateDistance(pairs, map) {
     let totalDistance = 0;
     let pair = 5;
     let from = pairs[pair][0];
     let to = pairs[pair][1];
+    // const galaxyMap = getGalaxyMap(expandedUniverse);
+    // // console.log(galaxyMap)
 
 
     for (let pair = 0; pair < pairs.length; pair++) {
         from = pairs[pair][0];
         to = pairs[pair][1];
         let distance = Math.abs(((map[to][1]) - (map[from][1]))) + Math.abs(((map[to][0]) - (map[from][0])));
-        console.log(`Distance is ${distance}`);
+        console.log(`Distance ${pair + 1} (${from} <-> ${to}) is ${distance}`);
+        // fs.appendFileSync('distances.txt', `Distance ${pair + 1} (${from} (${map[from]},${map[from]}) <-> ${to} (${map[to]},${map[to]})) is ${distance}\n`);
         totalDistance += distance;
     }
     console.log(totalDistance);
