cc9df9f50e2740d2f2a65cd8be3750365d045258
WillyVaessen <github@willyvaessen.nl>
2023-12-15 08:33:48 +0100

    Day 15. Finally a sense of success again:
    That's the right answer! You are one gold star closer to restoring snow operations.

---
+++ Day15/Day15_Part1.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+let currentValue = 0;
+// const inputString = 'HASH';
+const INPUT = fs.readFileSync('./Day15_Input', 'utf-8').split(',');
+// console.log(inputString);
+// const inputString = 'HASH';
+// console.log(string.charCodeAt(0));
+// console.log(string.charCodeAt(0));
+// console.log(string.charCodeAt(0));
+// console.log(string.charCodeAt(0));
+//
+
+function holidayHelper(string) {
+    console.log("**********************************************************");
+    const MULTIPLIER = 17;
+    const DIVIDER = 256;
+
+    let currentValue = 0;
+
+    let subTotal = 0;
+    // console.log(string);
+    for (let pos = 0; pos < string.length; pos++) {
+        // console.log(string[pos]);
+        let posASCIIValue = string.charCodeAt(pos);
+        console.log(`The current value starts at ${currentValue}`)
+        console.log(`1: The ${pos === 0 ? 'first' : 'next'} character is ${string[pos]}; its ASCII code is ${string.charCodeAt(pos)}.`)
+        console.log(`2: The current value increases to ${currentValue += string.charCodeAt(pos)}.`);
+        console.log(`3: The current value is multiplied by ${MULTIPLIER} to become ${currentValue * MULTIPLIER}.`);
+        console.log(`4: The current value becomes ${currentValue * MULTIPLIER % DIVIDER} (the remainder of ${currentValue * MULTIPLIER} divided by ${DIVIDER}).`);
+        currentValue = (currentValue * MULTIPLIER % DIVIDER);
+        console.log("---------------------------------------------------");
+        console.log(`Current value is now: ${currentValue}`);
+        console.log("---------------------------------------------------");
+
+    }
+    console.log("**********************************************************");
+return currentValue;
+}
+holidayHelper('HASH');
+
+console.log(INPUT)
+let total = 0;
+for (let i = 0; i < INPUT.length; i++) {
+
+    console.log(INPUT[i]);
+    let string = INPUT[i];
+    total += holidayHelper(string);
+    console.log(total);
+}
