4d92034e28a29eac92a548348a3a171b73f2d6f3
WillyVaessen <github@willyvaessen.nl>
2023-12-23 07:57:55 +0100

    Added day 23.
    Probably too little time to really work on it.

---
+++ Day23/Day23_Part1.js
@@ -0,0 +1,6 @@
+//  First get the input:
+const inputHelpers = require('../HelperFunctions/inputHelpers');
+const algos = require('../HelperFunctions/algos');
+const INPUT = inputHelpers.readInputFile('./Day23_Input');
+// const INPUT = inputHelpers.readInputFile('./Day23_Input_Example');
+console.log(INPUT)
