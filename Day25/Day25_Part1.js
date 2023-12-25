371f21ba9b0e5cc513a375ec7d9d5e4055ddbc6f
WillyVaessen <github@willyvaessen.nl>
2023-12-25 09:40:57 +0100

    Added day 25.
    Merry Christmas to you all.

---
+++ Day25/Day25_Part1.js
@@ -0,0 +1,6 @@
+//  First get the input:
+const inputHelpers = require('../HelperFunctions/inputHelpers');
+const algos = require('../HelperFunctions/algos');
+// const INPUT = inputHelpers.readInputFile('./Day25_Input');
+const INPUT = inputHelpers.readInputFile('./Day25_Input_Example');
+console.log(INPUT)
